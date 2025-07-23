32740b8870b483d8b4f4e140f909b2c26c0a9e88
Harish Pandey <149171752+HarishCPandey@users.noreply.github.com>
2025-07-23 22:40:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.37.0"
     }
   }
 }
-
 provider "azurerm" {
- features {}
- subscription_id = "subscription_id = "b397c2d0-369a-47dd-9100-9467b7447185""
+  features {}
+  subscription_id = "b397c2d0-369a-47dd-9100-9467b7447185"
 }
